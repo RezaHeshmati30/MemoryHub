4f44e3819ea5d718fc9292c62d25a1b96174138c
Mina <Mina@example.com>
2024-02-15 16:20:16 +0100

    slider with CSS

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -1,89 +1,8 @@
-// import React, { useContext, useEffect, useState } from "react";
-// import { StudySetsContext } from "../context/StudySetsContext";
-
-// function Cards() {
-//   const [flip, setFlip] = useState(false);
-//   const [currentIndex, setCurrentIndex] = useState(0);
-
-//   const { getStudySetData, topicId, cardsId, studySet } =
-//     useContext(StudySetsContext);
-
-//   useEffect(() => {
-//     getStudySetData();
-//     console.log("Topic Id:", topicId);
-//     console.log("Cards Id:", cardsId);
-//   }, []);
-
-//   const currentCardsSet = studySet?.topics
-//     ?.filter((topic) => topic._id === topicId)[0]
-//     ?.studySets.filter((set) => set._id === cardsId)[0];
-
-//   const currentCard = currentCardsSet.cards[currentIndex];
-
-//   const handleNextCard = () => {
-//     setCurrentIndex(
-//       (prevIndex) => (prevIndex + 1) % currentCardsSet?.cards.length
-//     );
-//     setFlip(false);
-//   };
-
-//   const handlePreviousCard = () => {
-//     setCurrentIndex(
-//       (prevIndex) =>
-//         (prevIndex - 1 + currentCardsSet?.cards.length) %
-//         currentCardsSet?.cards.length
-//     );
-//     setFlip(false);
-//   };
-//   const handleFlip = () => {
-//     setFlip((prevFlip) => !prevFlip);
-//   };
-
-//   return (
-//     <section>
-//       <ul>
-//         {currentCard && (
-//           <li key={currentCard._id}>
-//             <p>Title: {currentCardsSet.title}</p>
-//             <p>Description: {currentCardsSet.description}</p>
-//             <div className="w-[500px] h-[500px] bg-gray-200 m-10">
-//               <ul onClick={handleFlip} className='mt-[30px]'>
-//                 <li key={currentCard._id} className='mt-[20px]'>
-//                   {!flip ? (
-//                     <p >Answer: {currentCard.answer}</p>
-//                   ) : (
-//                     <p >
-//                       Question: {currentCard.question}
-//                     </p>
-//                   )}
-//                 </li>
-//               </ul>
-//             </div>
-//           </li>
-//         )}
-//       </ul>
-//       <button
-//         onClick={handlePreviousCard}
-//         className='bg-blue-500 m-5 p-2 rounded-md'
-//       >
-//         Previous
-//       </button>
-//       <button
-//         onClick={handleNextCard}
-//         className='bg-red-500 m-5 p-2 rounded-md'
-//       >
-//         Next
-//       </button>
-//     </section>
-//   );
-// }
-
-// export default Cards;
-
-import React, { useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { StudySetsContext } from "../context/StudySetsContext";
 import ReactCardFlip from "react-card-flip";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import "./cards.css";
 
 function Cards() {
   const [isFlipped, setIsFlipped] = useState(false);
@@ -122,135 +41,60 @@ function Cards() {
   };
 
   const handleFlip = () => {
-    setIsFlipped((prevIsFlipped) => !prevIsFlipped);
+    setIsFlipped(!isFlipped);
   };
 
   return (
     <section>
-      <section>
+      <section className='w-[80vw] h-[90vh] bg-gray-200 mx-auto my-5 px-10 py-10'>
         {currentCard && (
           <div>
-            <p>Title: {currentCardsSet.title}</p>
-            <p>Description: {currentCardsSet.description}</p>
-            <ReactCardFlip
-              isFlipped={isFlipped}
-              flipDirection='vertical'
-              flipSpeedBackToFront={1}
-              flipSpeedFrontToBack={1}
+            <p className='text-2xl'>Title: {currentCardsSet.title}</p>
+            <p className='my-5'>Description: {currentCardsSet.description}</p>
+            <div
+              className='flip-container flex justify-center '
+              onClick={handleFlip}
             >
-              <div key='front' className='m-[30px] ' onClick={handleFlip}>
-                <div className='m-[50px] w-[500px] h-[500px] bg-slate-300 text-center'>
-                  <p className='text-xl m-[50px] p-[100px]'>Question </p>
-                  <p className='text-l m-[50px]'> {currentCard.question}</p>
+              <div
+                className={`flip-card ${
+                  isFlipped ? "flipped" : ""
+                } w-[60vw] h-[50vh]`}
+              >
+                <div className='flip-content flex flex-row'>
+                  Question
+                  <p> {currentCard.question}</p>
+                </div>
+                <div className='flip-content flex flex-row bg-red-500'>
+                  <span className='text-2xl'>Answer</span>
+                  <p>{currentCard.answer}</p>
                 </div>
               </div>
-              <div key='back' className='m-[30px]' onClick={handleFlip}>
-                <div className='m-[50px] w-[500px] h-[500px] bg-slate-300 text-center'>
-                  <p className=' text-xl p-[100px]'>Answer</p>
-                  <p className='text-l m-[50px] '>{currentCard.answer}</p>
-                </div>
-              </div>
-            </ReactCardFlip>
+            </div>
+            <div className='my-5 ml-[110px]'>
+              <button
+                onClick={handlePreviousCard}
+                className='bg-gray-400  p-[10px] rounded-md'
+              >
+                Previous
+              </button>
+              <button
+                onClick={handleNextCard}
+                className='bg-gray-400 mx-10 p-[10px] rounded-md'
+              >
+                Next
+              </button>
+            </div>
           </div>
         )}
         <button
-          onClick={handlePreviousCard}
-          className='bg-blue-500 ml-[200px] p-[10px] rounded-md'
+          className='bg-blue-400 p-[10px] rounded-md'
+          onClick={() => navigate("/")}
         >
-          Previous
-        </button>
-        <button
-          onClick={handleNextCard}
-          className='bg-red-500 ml-[100px] p-[10px] rounded-md'
-        >
-          Next
+          back
         </button>
       </section>
-      <button onClick={() => navigate("/")}></button>
     </section>
   );
 }
 
 export default Cards;
-
-// import React, { useContext, useEffect, useState } from "react";
-// import { StudySetsContext } from "../context/StudySetsContext";
-
-// function Cards() {
-//   const [flip, setFlip] = useState(false);
-//   const [currentIndex, setCurrentIndex] = useState(0);
-
-//   const { getStudySetData, topicId, cardsId, studySet } =
-//     useContext(StudySetsContext);
-
-//   useEffect(() => {
-//     getStudySetData();
-//     console.log("Topic Id:", topicId);
-//     console.log("Cards Id:", cardsId);
-//   }, []);
-
-//   const currentCardsSet = studySet?.topics
-//     ?.filter((topic) => topic._id === topicId)[0]
-//     ?.studySets.filter((set) => set._id === cardsId)[0];
-
-//   const currentCard = currentCardsSet.cards[currentIndex];
-
-//   const handleNextCard = () => {
-//     setCurrentIndex(
-//       (prevIndex) => (prevIndex + 1) % currentCardsSet?.cards.length
-//     );
-//     setFlip(false);
-//   };
-
-//   const handlePreviousCard = () => {
-//     setCurrentIndex(
-//       (prevIndex) =>
-//         (prevIndex - 1 + currentCardsSet?.cards.length) %
-//         currentCardsSet?.cards.length
-//     );
-//     setFlip(false);
-//   };
-//   const handleFlip = () => {
-//     setFlip((prevFlip) => !prevFlip);
-//   };
-
-//   return (
-//     <section>
-//       <ul>
-//         {currentCard && (
-//           <li key={currentCard._id}>
-//             <p>Title: {currentCardsSet.title}</p>
-//             <p>Description: {currentCardsSet.description}</p>
-//             <div className="w-[500px] h-[500px] bg-gray-200 m-10">
-//               <ul onClick={handleFlip} className='mt-[30px]'>
-//                 <li key={currentCard._id} className='mt-[20px]'>
-//                   {!flip ? (
-//                     <p >Answer: {currentCard.answer}</p>
-//                   ) : (
-//                     <p >
-//                       Question: {currentCard.question}
-//                     </p>
-//                   )}
-//                 </li>
-//               </ul>
-//             </div>
-//           </li>
-//         )}
-//       </ul>
-//       <button
-//         onClick={handlePreviousCard}
-//         className='bg-blue-500 m-5 p-2 rounded-md'
-//       >
-//         Previous
-//       </button>
-//       <button
-//         onClick={handleNextCard}
-//         className='bg-red-500 m-5 p-2 rounded-md'
-//       >
-//         Next
-//       </button>
-//     </section>
-//   );
-// }
-
-// export default Cards;
---
+++ Frontend/src/context/CardContext.jsx
@@ -0,0 +1,9 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import cookie from "js-cookie"; 
+import axios from "axios";
+
+const CardContext = createContext();
+
+const CardContextProvider = ({ children }) => {
+    
+}
