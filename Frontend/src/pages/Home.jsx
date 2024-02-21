b0bd3e56aa95d7a0e1155a8f47215a2ab79ebaf0
DarynaBb <darinabilenko@gmail.com>
2024-02-21 10:11:28 +0100

    login/signUp buttons on the studySet(s) pages fixed

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import LoginForm from '../components/LoginForm'
-import SignUpForm from '../components/SignUpForm'
 import HomePageButtons from '../components/HomePageButtons'
 
 function Home() {
   return (
     <>
-      <LoginForm />
-      <SignUpForm />
       <HomePageButtons />
     </>
   )
