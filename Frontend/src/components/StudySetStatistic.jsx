6fa48351a5bc5c1d3c14efa94dd54043b00741ab
DarynaBb <darinabilenko@gmail.com>
2024-02-22 09:50:26 +0100

    round counter added

--- Frontend/src/components/StudySetStatistic.jsx
+++ Frontend/src/components/StudySetStatistic.jsx
@@ -1,12 +1,13 @@
-import React, { useContext, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react'
+import { useLocation, useParams } from 'react-router-dom';
 import { UserStudySetsContext } from '../context/UserStudySetsContext';
 import { AuthContext } from '../context/AuthContext';
 
 function StudySetStatistic() {
-    const {countCardsByStatus} = useContext(UserStudySetsContext);
+    const {countCardsByStatus, round} = useContext(UserStudySetsContext);
     const {getUserInfo, user} = useContext(AuthContext);
     const {id} = useParams();
+    const locate = useLocation();
 
     useEffect(() => {
         getUserInfo();
@@ -23,6 +24,7 @@ function StudySetStatistic() {
         <section className='p-[30px] flex flex-col gap-[20px]'>
                 {studySet && (
                     <>
+                        <h2 className={locate.pathname === `/studySet/endPractice/${id}` ? "block" : "hidden"}>Round: {round}</h2>
                         <h2>Topic: {studySet.topicTitle}</h2>
                         <h3>Title: {studySet.studySet.title}</h3>
                         <h3>Description: {studySet.studySet.description}</h3>
