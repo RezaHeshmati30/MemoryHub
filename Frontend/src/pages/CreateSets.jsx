123fda904e2720ee341c214bfb0113f335bf0bac
Mina <Mina@example.com>
2024-02-23 15:50:19 +0100

    in backend routes delete

--- Frontend/src/pages/CreateSets.jsx
+++ Frontend/src/pages/CreateSets.jsx
@@ -99,6 +99,21 @@ function CreateSets() {
           Create a New Study Set
         </h2>
         <div className='mb-4'>
+        <label
+            className='block text-gray-700 text-sm font-bold mb-2'
+            htmlFor='topic'
+          >
+            Topic
+          </label>
+          <input
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            id='topic'
+            type='text'
+            placeholder='Enter title'
+            name='topic'
+          />
+        </div>
+        <div className='mb-4'>
           <label
             className='block text-gray-700 text-sm font-bold mb-2'
             htmlFor='title'
