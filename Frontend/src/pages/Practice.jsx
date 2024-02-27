72929030a399b516bc4006f79da99a2f2b06ce30
reza <reza@gmail.com>
2024-02-27 10:01:03 +0100

    Refactor  Practice page ,CardFilter and PracticeButtons components

--- Frontend/src/pages/Practice.jsx
+++ Frontend/src/pages/Practice.jsx
@@ -12,83 +12,103 @@ function Practice() {
   const { id } = useParams();
   const [filterStatus, setFilterStatus] = useState('all');
   const [noCardsMessage, setNoCardsMessage] = useState('');
-
+  const [currentCardsSet, setCurrentCardsSet] = useState([]);
+  const [currentStudySet, setCurrentStudySet] = useState({});
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    getUserInfo();
-    setCurrentIndex(0);
-  }, []);
-
-  useEffect(() => {
-    getUserInfo();
-  }, [currentIndex]);
-
-  const studySet = user?.savedStudySets?.find(studySet => studySet._id === id) || {};
-  const currentCardsSet = studySet.cards || [];
-  const filteredCards = currentCardsSet.filter(card => {
-    if (filterStatus === 'all') {
-      return true;
-    } else if (filterStatus === 'not studied and need practice') {
-      return card.status === 'not studied' || card.status === 'need practice';
-    } else {
-      return card.status === filterStatus;
+    async function fetchData() {
+      await getUserInfo();
+      setCurrentIndex(0);
+      setLoading(false); // Set loading to false once user data is fetched
     }
-  });
-
+    fetchData();
+  }, []);
   useEffect(() => {
-    if (filteredCards.length === 0) {
+    if (user) {
+      const studySet = user.savedStudySets?.find(studySet => studySet._id === id) || {};
+      setCurrentStudySet(studySet);
+      setCurrentCardsSet(studySet.cards || []);
+    }
+  }, [user, id]);
+  useEffect(() => {
+    setCurrentIndex(0);
+  }, [filterStatus]);
+   useEffect(() => {
+    if (filterStatus === "all") {
+      getUserInfo();
+    const studySet = user?.savedStudySets?.find(studySet => studySet._id === id) || {};
+    setCurrentStudySet(studySet);
+      const allCards = studySet?.cards || [];
+      setCurrentCardsSet(allCards);
+    }
+  }, [currentIndex]);
+  useEffect(() => {
+    if (currentCardsSet.length === 0) {
       setNoCardsMessage('No cards available for the selected filter.');
     } else {
       setNoCardsMessage('');
     }
-  }, [filteredCards]);
-
-  const currentCard = filteredCards[currentIndex];
-
-
+  }, [currentCardsSet]);
+  const currentCard = currentCardsSet[currentIndex];
   const handleFlip = () => {
     setIsFlipped(!isFlipped);
   };
-
-  const handleFilterChange = (status) => {
-    setFilterStatus(status);
-    const firstIndexWithStatus = filteredCards.findIndex(card => card.status === status);
-    setCurrentIndex(firstIndexWithStatus !== -1 ? firstIndexWithStatus : 0);
-  };
-
+  const handleFilterChange = (e) => {
+    const selectedValue = e.target.value;
+    setFilterStatus(selectedValue);
+    let filteredCards = currentStudySet.cards || [];
+    if (selectedValue === "mastered") {
+      filteredCards = filteredCards.filter(card => card.status === "mastered");
+    } else if (selectedValue === "need practice") {
+      filteredCards = filteredCards.filter(card => card.status === "need practice");
+    } else if (selectedValue === "not studied") {
+      filteredCards = filteredCards.filter(card => card.status === "not studied");
+    } else if (selectedValue === "not studied and need practice") {
+      filteredCards = filteredCards.filter(card => card.status === "need practice" || card.status === "not studied");
+    }
+    setCurrentCardsSet(filteredCards);
+  }
   return (
-    <div>
-      <section className='w-[80vw] h-[90vh] bg-gray-200 mx-auto my-5 px-10 py-10'>
-        <CardFilter filterStatus={filterStatus} setFilterStatus={setFilterStatus} handleFilterChange={handleFilterChange} /> {/* Filterkomponente verwenden */}
-        {noCardsMessage && <p>{noCardsMessage}</p>}
-        {filteredCards.length > 0 && (
-          <div key={studySet._id}>
-            <p>Title: {studySet?.studySet?.title}</p>
-            <p>Description: {studySet?.studySet?.description}</p>
-            <p>Status: {currentCard?.status}</p>
-            <div className='flip-container flex justify-center' onClick={handleFlip}>
-              <div className={`flip-card ${isFlipped ? "flipped" : ""} w-[60vw] h-[50vh]`}>
-                <div className='flip-content'>
-                  <strong>Question</strong>
-                  <p>{currentCard?.card?.question}</p>
+    !loading && (
+      <div>
+        <section className='w-[80vw] h-[90vh] bg-gray-200 mx-auto my-5 px-10 py-10'>
+        <CardFilter
+            filterStatus={filterStatus}
+            setFilterStatus={setFilterStatus}
+            handleFilterChange={handleFilterChange}
+          />
+          {noCardsMessage && <p>{noCardsMessage}</p>}
+          {currentCardsSet?.length > 0 && (
+            <>
+              <div key={currentStudySet._id}>
+                <p>Title: {currentStudySet?.studySet?.title}</p>
+                <p>Description: {currentStudySet?.studySet?.description}</p>
+                <p className={filterStatus === "all" ? "block" : "hidden"}>Status: {currentCard?.status}</p>
+                <div className='flip-container flex justify-center' onClick={handleFlip}>
+                  <div className={`flip-card ${isFlipped ? "flipped" : ""} w-[60vw] h-[50vh]`}>
+                    <div className='flip-content'>
+                      <strong>Question</strong>
+                      <p>{currentCard?.card?.question}</p>
+                    </div>
+                    <div className='flip-content'>
+                      <strong>Answer</strong>
+                      <p>{currentCard?.card?.answer}</p>
+                    </div>
+                  </div>
                 </div>
-                <div className='flip-content'>
-                  <strong>Answer</strong>
-                  <p>{currentCard?.card?.answer}</p>
+                <div className='my-5 ml-[110px] flex gap-[10px]'>
+                  <button onClick={() => handlePreviousCard(currentCardsSet)} className='bg-gray-400 p-[10px] rounded-md'>Previous</button>
+                  <p>{currentIndex + 1} / {currentCardsSet?.length}</p>
+                  <button onClick={() => handleNextCard(currentCardsSet)} className='bg-gray-400 mx-10 p-[10px] rounded-md'>Next</button>
                 </div>
               </div>
-            </div>
-            <div className='my-5 ml-[110px] flex gap-[10px]'>
-              <button onClick={() => handlePreviousCard(filteredCards)} className='bg-gray-400 p-[10px] rounded-md'>Previous</button>
-              <p>{currentIndex + 1} / {filteredCards?.length}</p>
-              <button onClick={() => handleNextCard(filteredCards)} className='bg-gray-400 mx-10 p-[10px] rounded-md'>Next</button>
-            </div>
-          </div>
-        )}
-        <button className='bg-blue-400 p-[10px] rounded-md' onClick={() => navigate("/user/studySets")}>Back to Study Sets</button>
-        <PracticeButtons />
-      </section>
-    </div>
+            </>
+          )}
+          <button className='bg-blue-400 p-[10px] rounded-md' onClick={() => navigate("/user/studySets")}>Back to Study Sets</button>
+          <PracticeButtons currentSet={currentCardsSet} />
+        </section>
+      </div>
+    )
   );
 }
-
 export default Practice;
