b80b1312575c82cffd116309a40814b582eb35db
reza <reza@gmail.com>
2024-03-01 13:53:47 +0100

    Update logoutController and VITE_SERVER_URL

--- Backend/controllers/logoutController.js
+++ Backend/controllers/logoutController.js
@@ -1,7 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const postLogoutController = async (req, res) => {
-    res.clearCookie("jwt");
-    res.clearCookie("JWTinfo");
-    res.send({msg: "erfolgreich ausgeloggt"});
-}
+  res.clearCookie("jwt", {
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    secure: process.env.NODE_ENV === "production",
+  });
+  res.clearCookie("JWTinfo", {
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    secure: process.env.NODE_ENV === "production",
+  });
+  res.send({ msg: "erfolgreich ausgeloggt" });
+};
