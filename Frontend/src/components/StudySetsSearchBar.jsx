24fb842c0d1326804185e39212d6beebc06ea80b
DarynaBb <darinabilenko@gmail.com>
2024-03-11 12:14:54 +0100

    search bar styled

--- Frontend/src/components/StudySetsSearchBar.jsx
+++ Frontend/src/components/StudySetsSearchBar.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import search from "../assets/images/search.svg"
 
 const StudySetsSearchBar = ({ value, onChange }) => {
+    const [onFocus, setOnFocus] = useState(false);
+
     return (
-      <form action="" className='flex justify-between w-full'>
+      <form action="" className='flex justify-center w-full mb-[4em]'>
+        <div className='flex justify-between basis-1/2 bg-[#F6F7FB] border-[1px] border-black p-[1.5em] rounded-[8px]'>
         <input
-        type="text"
-        placeholder="Search..."
-        value={value}
-        onChange={onChange}
-        className='block basis-1/2  border-[1px] border-black p-[1.5em] rounded-[8px] mb-4'
+          onFocus={() => setOnFocus(true)}
+          onBlur={() => setOnFocus(false)}
+          type="text"
+          placeholder="Search..."
+          value={value}
+          onChange={onChange}
+          className='outline-none block w-full bg-[#F6F7FB]'
       />
-      <img src={search} alt="" />
+      <img src={search} alt="" /> 
+        </div>
+       
       </form>
       
     );
