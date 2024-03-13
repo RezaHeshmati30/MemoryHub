d128df213eb7ced5d4552a1d7c786a90dfa606e2
DarynaBb <darinabilenko@gmail.com>
2024-03-13 12:01:45 +0100

    foreign user page styled, search bar does not work

--- Frontend/src/pages/ForeignUserStudySets.jsx
+++ Frontend/src/pages/ForeignUserStudySets.jsx
@@ -1,11 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { StudySetsContext } from '../context/StudySetsContext';
 import { useNavigate, useParams } from 'react-router-dom';
+import StudySetsSearchBar from '../components/StudySetsSearchBar';
+import BackLink from '../components/BackLink';
+import arrow from "../assets/images/arrow-forward.svg";
 
 function ForeignUserStudySets() {
     const { getUserStudySets, userStudySets, getUserShortData, userShortData, setTopicId, setStudySetId } = useContext(StudySetsContext);
     const { userId } = useParams();
     const [groupedStudySets, setGroupedStudySets] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -16,19 +20,70 @@ function ForeignUserStudySets() {
     console.log("userStudysets", userStudySets)
 
     useEffect(() => {
-        if (userStudySets && userStudySets?.savedStudySets) {
-            const grouped = {};
-            userStudySets?.savedStudySets?.forEach(studySet => {
-                const topicTitle = studySet?.topic?.title; // Check if topic exists
-                if (!grouped[topicTitle]) {
-                    grouped[topicTitle] = [];
+        // if (userStudySets && userStudySets?.savedStudySets) {
+        //     const grouped = {};
+        //     userStudySets?.savedStudySets?.forEach(studySet => {
+        //         const topicTitle = studySet?.topic?.title; // Check if topic exists
+        //         if (!grouped[topicTitle]) {
+        //             grouped[topicTitle] = [];
+        //         }
+        //         grouped[topicTitle].push(studySet);
+        //     });
+        //     setGroupedStudySets(grouped);
+
+        // }
+        const filteredStudySets = userStudySets?.savedStudySets?.reduce((acc, studySet) => {
+            const topicTitle = studySet?.topic?.title;
+            const description = studySet?.studySet?.description || ''; // Consider empty string if description is undefined
+        
+            if (
+                topicTitle?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                description?.toLowerCase().includes(searchQuery.toLowerCase())
+            ) {
+                const existingTopicIndex = acc.findIndex(item => item.title === topicTitle);
+                if (existingTopicIndex !== -1) {
+                    acc[existingTopicIndex].studySets.push(studySet);
+                } else {
+                    acc.push({ title: topicTitle, studySets: [studySet] });
                 }
-                grouped[topicTitle].push(studySet);
-            });
-            setGroupedStudySets(grouped);
-        }
+            }
+            return acc;
+        }, []);
+        
+        setGroupedStudySets(filteredStudySets);
+        
+
+        // const filteredStudySets = userStudySets?.savedStudySets?.reduce((acc, studySet) => {
+        //     const topicTitle = studySet?.topic?.title;
+        //     const description = studySet?.studySet?.description || '';
+        
+        //     if (
+        //         topicTitle?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        //         description?.toLowerCase().includes(searchQuery.toLowerCase())
+        //     ) {
+        //         acc.push(studySet);
+        //     }
+        //     return acc;
+        // }, []);
+        
+        // setGroupedStudySets(filteredStudySets);
+        
+        
+        console.log('Search Query:', searchQuery);
+        
     }, [userStudySets]);
 
+    console.log('Search Query:', searchQuery);
+    // const filteredStudySets = userStudySets?.topics?.reduce((acc, topic) => {
+    //     const filteredSets = topic.studySets.filter(studySet => studySet.title.toLowerCase().includes(searchQuery.toLowerCase()));
+    //     if (filteredSets.length) {
+    //       acc.push({ ...topic, studySets: filteredSets });
+    //     }
+    //     return acc;
+    //   }, []);
+    
+    
+console.log("gg:", groupedStudySets)
     const onClickHandler = (topicId, studySetId) => {
         navigate(`/users/${userId}/topic/${topicId}/study-set/${studySetId}`);
         // setTopicId(topicId);
@@ -37,26 +92,40 @@ function ForeignUserStudySets() {
 
     return (
         <section className='p-[40px]'>
+            <BackLink />
             {userStudySets && userShortData && (
                 <div>
-                    <div className='flex items-center gap-[10px] mb-[20px]'>
-                        <img className='w-[100px] h-[100px] rounded-[50%]' src={userShortData?.photo} alt="" />
-                        <h2>Study sets of <span className='font-bold'>{userShortData?.nickName}</span></h2>
+                    <div className='flex justify-center items-center gap-[10px] mb-[42px]'>
+                        <img className='w-[60px] h-[60px] rounded-[50%]' src={userShortData?.photo} alt="" />
+                        <h2 className='text-[4em]'>{userShortData?.nickName}</h2>
                     </div>
-                    <ul className='flex gap-[15px] flex-wrap'>
-                        {Object.entries(groupedStudySets).map(([topic, studySets]) => (
-                            <li key={topic} className='border-[2px] border-gray-400 p-[10px] basis-1/3'>
-                                <h3 className='mb-[10px]'>{console.log("topic:",studySets)}{topic}</h3>
-                                <ul className='flex flex-col gap-[10px]'>
-                                    {studySets.map(studySet => (
-                                        <li key={studySet._id} onClick={() => onClickHandler(studySet.topic._id, studySet.studySet._id)} className='border-[1px] rounded-[8px] border-gray-400 p-[10px]'>
-                                            {studySet.studySet && ( 
-                                                <>
-                                                    <p>Title:{studySet.studySet.title}</p>
-                                                    <p>Description: {studySet.studySet.description}</p>
-                                                </>
-                                            )}
-                                        </li>
+                    <h3 className='text-center mb-[32px] dm-sans-medium text-[1.4em] text-leadong-[150%]'>ALL STUDY SETS</h3>
+                    <div className='flex flex-col w-full items-center justify-center mb-[48px] mx-auto'>
+                        <div className='w-[80%] h-[1px] bg-[#E1E1E1]'/>
+                        <div className='w-[15%] h-[1px] relative z-3 -mt-[1px] bg-black'/>
+                    </div>
+                    <StudySetsSearchBar value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+                    <ul className='flex flex-col gap-[25px] w-full items-center justify-center'>
+                        {groupedStudySets?.map(({ title, studySets }) => (
+                            <li key={title} className='w-[50%]'>
+                                <div className='flex items-center gap-[4px] border-b-[1px] border-[#E1E1E1] pb-[14px]'>
+                                    <h3 className='dm-sans-bold text-[2em]'>{title}</h3>
+                                    <img src={arrow} className='rotate-45' alt="arrow" />
+                                </div>
+                                
+                                <ul className='flex flex-col w-full'>
+                                    {studySets?.map(studySet => (
+                                    <li key={studySet?._id} onClick={() => onClickHandler(studySet.topic._id, studySet.studySet._id)} className='border-b-[1px] w-full hover:bg-[#69CA61] cursor-pointer border-[#E1E1E1] py-[14px]'>
+                                        {studySet?.studySet && ( 
+                                            <div className='flex w-full justify-between items-center'>
+                                                <p className='dm-sans-medium text-[1.7em] text-leading-[150%]'>{studySet?.studySet?.title}</p>
+                                                <div className='flex hover:underline items-center gap-[8px] cursor-pointer'>
+                                                    <button className='dm-sans-bold text-[1.2em] uppercase' onClick={() => onClickHandler(studySet.topic._id, studySet.studySet._id)}>Go to set</button>
+                                                    <img src={arrow} alt="arrow" />
+                                                </div>
+                                            </div>
+                                        )}
+                                    </li>
                                     ))}
                                 </ul>
                             </li>
